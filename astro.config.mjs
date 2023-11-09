a7c5ea400c21434f307cf3b492f76425521de794
jonasnoki <Jonas.Noack@student.hpi.uni-potsdam.de>
2023-11-09 21:09:32 +0100

    switch to server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), sitemap()],
-    output: "static",
+    output: "server",
     site: "https://jonasnoack.dev",
     server: {
         port: 3000,
